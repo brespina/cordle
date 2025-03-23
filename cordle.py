92e77b869dedf534a9e7f579fca21303efd0c211
brespina <brespina4@gmail.com>
2025-03-23 00:30:27 -0500

    init: cordle

---
+++ cordle.py
@@ -0,0 +1,13 @@
+"""
+__author__: bwandii
+__created__: 10/10/2024
+__description__: a for fun coding project where i make the game wordle
+                 but in discord
+"""
+
+import discord
+from discord.ext import commands
+import json
+import random
+# you will need your own bot token
+from cordle_token import get_token
---
+++ cordle_token.py
@@ -0,0 +1,5 @@
+token = ''
+
+
+def get_toke():
+    return token
